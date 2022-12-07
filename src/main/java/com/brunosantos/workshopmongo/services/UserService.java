f3a8c93a179803b03a5c81db4409fa0d827fe3ae
bruno santos <bruno4t@hotmail.com>
2022-12-07 09:43:17 -0300

    obtendo usuario por id e fazendo tratamento de erros

--- src/main/java/com/brunosantos/workshopmongo/services/UserService.java
+++ src/main/java/com/brunosantos/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.brunosantos.workshopmongo.services;
 
 import com.brunosantos.workshopmongo.domain.User;
 import com.brunosantos.workshopmongo.repository.UserRepository;
+import com.brunosantos.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,13 @@ public class UserService {
     public List<User> findALl(){
         return repo.findAll();
     }
+
+   public User findById(String id){
+        User user = repo.findById(id).orElse(null);
+        if(user == null){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user;
+   }
 
 }
