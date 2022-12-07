468d7890acf30d05613c3c1d50d042dd5d50746e
bruno santos <bruno4t@hotmail.com>
2022-12-07 08:22:26 -0300

    criação do projeto spring

---
+++ src/main/java/com/brunosantos/workshopmongo/WorkshopMongodbSpringApplication.java
@@ -0,0 +1,13 @@
+package com.brunosantos.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongodbSpringApplication.class, args);
+	}
+
+}
